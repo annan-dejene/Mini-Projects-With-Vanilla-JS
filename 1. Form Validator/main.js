ae45de183cbd12ece56dd7162b7bc647553440f7
Annan Dejene <annandejene2020@gmail.com>
2021-09-03 17:15:31 -0700

    Gave a basic functionality for the form

--- 1. Form Validator/main.js
+++ 1. Form Validator/main.js
@@ -1,2 +1,62 @@
 
-const form = document.getElementById('')
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+// Functions 
+
+// 1. Show input error message
+function showError(input, message) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control error'; // Same as formControl.classList.add('error');
+    const small = formControl.querySelector('small');
+    small.innerText = message;
+}
+
+// 2. Show success outline
+function showSuccess(input) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control success';
+}
+
+// 3. Validate email
+function isvalidEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+// Event Listener
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+
+    if (username.value === '') {
+        showError(username, 'Username is required')
+    } else {
+        showSuccess(username);
+    }
+
+    if (email.value === '') {
+        showError(email, 'Email is required')
+    } else if (!isvalidEmail(email.value)) {
+        showError(email, 'Email is not valid')
+    } else {
+        showSuccess(email);
+    }
+
+    if (password.value === '') {
+        showError(password, 'Password is required')
+    } else {
+        showSuccess(password);
+    }
+
+    if (password2.value === '') {
+        showError(password2, 'Password 2 is required')
+    } else {
+        showSuccess(password2);
+    }
+    
+
+});
