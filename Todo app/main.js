e5fb773bdce18794f71b34487dfacc169eb091f9
Annan Dejene <annandejene2020@gmail.com>
2021-09-05 16:52:35 -0700

    Added a filter for the todos

--- Todo app/main.js
+++ Todo app/main.js
@@ -2,6 +2,7 @@
 const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
+const filterOption = document.querySelector('.filter-todo');
 
 // Functions
 function addTodo(event) {
@@ -49,6 +50,34 @@ function deleteCheck(event) {
     }
 }
 
+
+function filterTodo(event) {
+    const todos = todoList.childNodes;
+    todos.forEach(function(todo){
+        
+        switch (event.target.value) {
+            case 'all':
+                todo.style.display = 'flex';
+                break;
+            case 'completed':
+                if (todo.classList.contains('completed')) {
+                    todo.style.display = 'flex';
+                } else {
+                    todo.style.display = 'none';
+                }
+                break;
+            case 'uncompleted':
+                if (!todo.classList.contains('completed')) {
+                    todo.style.display = 'flex';
+                } else {
+                    todo.style.display = 'none';
+                }
+                break;
+        }
+    });
+}
+
 // Event Listner
 todoButton.addEventListener('click', addTodo);
 todoList.addEventListener('click', deleteCheck);
+filterOption.addEventListener('click', filterTodo);
