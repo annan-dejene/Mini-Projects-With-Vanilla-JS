be22c075aed54ed6dfde5c03d24334519482802c
Annan Dejene <annandejene2020@gmail.com>
2021-09-05 14:35:18 -0700

    Gave the todo app a basic functionality

---
+++ Todo app/main.js
@@ -0,0 +1,54 @@
+// Selectors
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+
+// Functions
+function addTodo(event) {
+    // Prevent form from submitting
+    event.preventDefault();
+    // Create a Todo div
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo');
+    // Create li
+    const newTodo = document.createElement('li');
+    newTodo.innerText = todoInput.value;
+    newTodo.classList.add('todo-item');
+
+    todoDiv.appendChild(newTodo);
+    // Checkmark button
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class="fas fa-check"></i>'
+    completedButton.classList.add('completed-btn');
+    todoDiv.appendChild(completedButton);
+    // Trash button
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>'
+    trashButton.classList.add('trash-btn');
+    todoDiv.appendChild(trashButton);
+    // Append to Todo List
+    todoList.appendChild(todoDiv);
+    // Clear Todo Input value
+    todoInput.value = '';
+}
+
+function deleteCheck(event) {
+    const clickedItem = event.target;
+    // Delete Todo
+    if (clickedItem.classList[0] === 'trash-btn') {
+        const todo = clickedItem.parentElement;
+        todo.classList.add('fall');
+        todo.addEventListener('transitionend', function(){
+            todo.remove();
+        });
+    }
+
+    // Check mark
+    if (clickedItem.classList[0] === 'completed-btn') {
+        clickedItem.parentElement.classList.toggle('completed');
+    }
+}
+
+// Event Listner
+todoButton.addEventListener('click', addTodo);
+todoList.addEventListener('click', deleteCheck);
