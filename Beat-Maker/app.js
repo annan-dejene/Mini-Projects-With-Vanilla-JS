98631e7906099b54aafd464ff66d2e2fd96ba803
Annan Deriba <annandejene2020@gmail.com>
2024-06-30 13:10:21 +0300

    Added sound to play for each pads

---
+++ Beat-Maker/app.js
@@ -0,0 +1,60 @@
+class DrumKit {
+  constructor() {
+    this.pads = document.querySelectorAll(".pad");
+    this.playBtn = document.querySelector(".play");
+    this.kickAudio = document.querySelector(".kick-sound");
+    this.snareAudio = document.querySelector(".snare-sound");
+    this.hihatAudio = document.querySelector(".hihat-sound");
+    this.index = 0;
+    this.bpm = 250; // (Beats per minute) To control the speed of the sound
+  }
+
+  activePad() {
+    this.classList.toggle("active");
+  }
+
+  repeat() {
+    let step = this.index % 8;
+    const activeBars = document.querySelectorAll(`.b${step}`);
+    // loop over the pads
+    activeBars.forEach((bar) => {
+      bar.style.animation = `playTrack 0.3s alternate ease-in-out 2`; // playTrack is the animation name we created in the css file, 0.3s is the duration of the animation, alternate is the direction of the animation, ease-in-out is the timing function, 2 is the number of times the animation will play (1 time forward and 1 time backward)
+
+      // Check if the bar is active
+      if (bar.classList.contains("active")) {
+        // Check which sound to play
+        if (bar.classList.contains("kick-pad")) {
+          this.kickAudio.currentTime = 0; // (Restarts the time of the actual audio track) To play the sound from the beginning every time it's turn comes
+          this.kickAudio.play();
+        } else if (bar.classList.contains("snare-pad")) {
+          this.snareAudio.currentTime = 0; // To play the sound from the beginning every time it's turn comes
+          this.snareAudio.play();
+        } else if (bar.classList.contains("hihat-pad")) {
+          this.hihatAudio.currentTime = 0; // To play the sound from the beginning every time it's turn comes
+          this.hihatAudio.play();
+        }
+      }
+    });
+    this.index++;
+  }
+
+  start() {
+    const interval = (60 / this.bpm) * 1000;
+    setInterval(() => {
+      this.repeat();
+    }, interval);
+  }
+}
+
+const drumKit = new DrumKit();
+
+drumKit.pads.forEach((pad) => {
+  pad.addEventListener("click", drumKit.activePad);
+  pad.addEventListener("animationend", function () {
+    this.style.animation = "";
+  });
+});
+
+drumKit.playBtn.addEventListener("click", () => {
+  drumKit.start();
+});
