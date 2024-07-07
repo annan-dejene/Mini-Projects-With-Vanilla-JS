70b9aae0713725fbd7edc281bf54b82ccd6484c5
Annan Deriba <annandejene2020@gmail.com>
2024-07-07 17:53:58 +0300

    Colorized sliders

--- Coloors/app.js
+++ Coloors/app.js
@@ -34,6 +34,15 @@ function randomColors() {
 
     // Check for text contrast relative to the div's background color
     checkTextContrast(randomColor, hexText);
+
+    // Initial colorize sliders
+    const color = chroma(randomColor);
+    const sliders = div.querySelectorAll(".sliders input");
+    const hue = sliders[0];
+    const brightness = sliders[1];
+    const saturation = sliders[2];
+
+    colorizeSliders(color, hue, brightness, saturation);
   });
 }
 
@@ -44,6 +53,28 @@ function checkTextContrast(color, text) {
   } else {
     text.style.color = "white";
   }
+}
+
+function colorizeSliders(color, hue, brightness, saturation) {
+  // Scale saturation
+  const noSat = color.set("hsl.s", 0); // min saturation
+  const fullSat = color.set("hsl.s", 1); // max saturation
+  const scaleSaturation = chroma.scale([noSat, color, fullSat]);
+
+  // Scale brightness
+  const midBright = color.set("hsl.l", 0.5); // we are doing only mid Brightness because we know highest brightness is full light and none is dark
+  const scaleBrightness = chroma.scale(["black", midBright, "white"]);
+
+  // Update input slider colors
+  saturation.style.backgroundImage = `linear-gradient(to right, ${scaleSaturation(
+    0
+  )}, ${scaleSaturation(1)})`;
+
+  brightness.style.backgroundImage = `linear-gradient(to right, ${scaleBrightness(
+    0
+  )}, ${scaleBrightness(0.5)}, ${scaleBrightness(1)})`;
+
+  hue.style.backgroundImage = `linear-gradient(to right, rgb(204,75,75),rgb(204,204,75),rgb(75,204,75),rgb(75,204,204),rgb(75,75,204),rgb(204,75,204),rgb(204,75,75))`;
 }
 
 randomColors();
